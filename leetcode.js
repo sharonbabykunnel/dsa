901535a5cec81b0d359d71434216fa21cdaa68a9
Sharon <sharonkunnel2003@gmail.com>
2024-03-18 22:55:54 +0530

    ddd

--- leetcode.js
+++ leetcode.js
@@ -27,3 +27,37 @@ const isMatch = function (string, pattern) {
   }
   return true;
 };
+
+
+var minimumOperationsToWriteY = function (grid) {
+  let countY = Array(3).fill(0);
+  let countOutside = Array(3).fill(0);
+  let n = grid.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (isPartOfY(i, j, n)) {
+        countY[grid[i][j]]++;
+      } else {
+        countOutside[grid[i][j]]++;
+      }
+    }
+  }
+  let ans = Infinity;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (i === j) continue;
+      let costY = countY[0] + countY[1] + countY[2] - countY[i];
+      let costOutsideY =
+        countOutside[0] + countOutside[1] + countOutside[2] - countOutside[j];
+      ans = Math.min(ans, costY + costOutsideY);
+    }
+  }
+  return ans;
+};
+
+function isPartOfY(row, col, n) {
+  let isLeftPart = row <= Math.floor(n / 2) && row === col;
+  let isRightPart = row <= Math.floor(n / 2) && row + col === n - 1;
+  let isMiddlePart = row >= Math.floor(n / 2) && col === Math.floor(n / 2);
+  return isLeftPart || isRightPart || isMiddlePart;
+}
